ed9c66ff8a950d3d8c204fcf836d59cdf8e2470d
h-fahad <64426351+h-fahad@users.noreply.github.com>
2021-03-10 20:56:20 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+     <h1>my name is {props.name}. I am {props.age-10}</h1>
+    </div>
+  );
+}
+
+export default App;
